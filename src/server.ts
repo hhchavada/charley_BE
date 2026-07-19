cf75dc921db2f19e09bbcf42309c1299180e3499
harsh <harshchavada174@gmail.com>
2026-07-19 10:26:14 +0530

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = process.env.PORT || 3007;
+const PORT = process.env.PORT || 4002;
 
 app.listen(PORT, () => {
   console.log(`GrantMatch AI Backend is running on http://localhost:${PORT}`);
